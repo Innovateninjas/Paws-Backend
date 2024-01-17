f004b4ae822a51683ae7b87ecb942d5d923409ed
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-01-18 03:42:22 +0530

    Update database settings and add psycopg2-binary dependency

--- backend/settings.py
+++ backend/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "verceldb",
+        'USER': "default",
+        'PASSWORD': "pUkWcPu2Dqw3",
+        'HOST': "ep-wispy-mud-64585382-pooler.ap-southeast-1.postgres.vercel-storage.com",
     }
 }
 
