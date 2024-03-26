b3ece762190182e847af70e949d1db9bee5d093b
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-03-26 20:30:51 +0530

    Add image to notification

--- aniresfr/notify.py
+++ aniresfr/notify.py
@@ -7,6 +7,7 @@ def send_notification(notify_list=[], title='title', body='body'):
         notification=messaging.Notification(
             title=title,
             body=body,
+            image="https://pawss.vercel.app/logo512.png"
         ),
         tokens=notify_list,
     )
