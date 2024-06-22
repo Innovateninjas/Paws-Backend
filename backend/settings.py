8556aa62ab725c9eb9fd671b66bbf7e8d73e5641
Arnab Mondal <dp950810@gmail.com>
2024-06-22 12:47:39 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -72,10 +72,14 @@ if RUNNING_DEVSERVER or DEBUG:
     ALLOWED_HOSTS += ['localhost', '127.0.0.1']
     CORS_ALLOWED_ORIGINS += ['http://localhost:8000',
                              'http://127.0.0.1:8000',
+                             'http://localhost:5173',
+                             'http://127.0.0.1:5173',
                              'http://localhost:3000',
                              'http://127.0.0.1:3000']
     CSRF_TRUSTED_ORIGINS += ['http://localhost:8000',
                              'http://127.0.0.1:8000',
+                             'http://localhost:5173',
+                             'http://127.0.0.1:5173',
                              'http://localhost:3000',
                              'http://127.0.0.1:3000']
 
